e1798da99e4f45ae8f34dd06c7d24cb3143a34c7
Vitalii Yevtushenko <vitalii@rollncode.com>
2015-10-22 15:21:12 +0300

    Initial Commit

---
+++ TestAppcodePCH/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestAppcodePCH
+//
+//  Created by Vitaly Evtushenko on 10/22/15.
+//  Copyright © 2015 Roll'n'Code (rollncode.com). All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestAppcodePCH/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TestAppcodePCH
+//
+//  Created by Vitaly Evtushenko on 10/22/15.
+//  Copyright © 2015 Roll'n'Code (rollncode.com). All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
